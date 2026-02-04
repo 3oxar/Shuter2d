6de65e9aaf8da4e748ad818c047523878c7ccfb6
3oxap <141042005+3oxar@users.noreply.github.com>
2026-02-04 11:13:15 +0300

    Создан проект
    Сделано меню игры и открытие сцены уровня

---
+++ Shuter2d/Assets/Script/Scene/OpenScene.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Открытие сцены
+/// </summary>
+public class OpenScene : MonoBehaviour
+{
+    public int NumberScene;
+
+    /// <summary>
+    /// Открывает сцену по ее номеру
+    /// </summary>
+    public void Open()
+    {
+        SceneManager.LoadScene(NumberScene);
+    }
+}
