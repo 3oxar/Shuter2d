377047688d029a3aeaf9f7d1a897666697730cad
3oxap <141042005+3oxar@users.noreply.github.com>
2026-02-05 11:56:47 +0300

    Стрельба в сторону мышки

--- Shuter2d/Assets/Script/Bullet/FlyBullet.cs
+++ Shuter2d/Assets/Script/Bullet/FlyBullet.cs
@@ -4,16 +4,21 @@ public class FlyBullet : MonoBehaviour
 {
     [SerializeField] private float _speed = 10;
     [SerializeField] private int damage = 1;
-    void Start()
+    private Rigidbody2D _rb;
+
+    void Awake()
     {
-        GetComponent<Rigidbody2D>().linearVelocity = transform.right * _speed;
+        _rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, 2f);
+    }
+
+    public void Launch(Vector2 direction)
+    {
+        _rb.linearVelocity = direction.normalized * _speed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-
         Destroy(gameObject);
     }
 }
