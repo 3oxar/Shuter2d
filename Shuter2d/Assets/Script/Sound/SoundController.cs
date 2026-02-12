8d8b61e6839204e3d754080d95739b4c221107d2
3oxap <141042005+3oxar@users.noreply.github.com>
2026-02-12 08:34:50 +0300

    Сохранение значения звуков

--- Shuter2d/Assets/Script/Sound/SoundController.cs
+++ Shuter2d/Assets/Script/Sound/SoundController.cs
@@ -5,36 +5,50 @@ using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour
 {
-    [SerializeField] private List<AudioSource> _audioSource;
-    [SerializeField] private List<AudioSource> _audioSMusic;
+    [SerializeField] private List<AudioSource> _audioMusic;
+    [SerializeField] private List<AudioSource> _audioSound;
 
-    private float _volumeSave;
-    private Slider _slider;
+    private float _volumeSaveMusic;
+    private float _volumeSaveSound;
+    private Slider _sliderMusic;
+    private Slider _sliderSound;
 
     private void Awake()
     {
-        _volumeSave = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
-
-        foreach (var item in _audioSource)
+        _volumeSaveMusic = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        _volumeSaveSound = PlayerPrefs.GetFloat("MusicSound", 0.5f);
+        
+        foreach (var item in _audioMusic)
         {
-            item.volume = _volumeSave;
+            item.volume = _volumeSaveMusic;
         }
-        foreach (var item in _audioSMusic)
+        foreach (var item in _audioSound)
         {
-            item.volume = _volumeSave;
+            item.volume = _volumeSaveSound;
         }
-
-        _slider.onValueChanged.AddListener(delegate { UpdateValue(); });
+        _sliderMusic.onValueChanged.AddListener(delegate { UpdateMusicValue(); });
+        _sliderSound.onValueChanged.AddListener(delegate { UpdateSoundValue(); });
     }
 
-    public void UpdateValue()
+    public void UpdateMusicValue()
     {
-        foreach (var item in _audioSource)
+        foreach (var item in _audioSound)
         {
-            item.volume = _slider.value;
+            item.volume = _sliderMusic.value;
         }
 
-        PlayerPrefs.SetFloat("MusicVolume", _slider.value);
+        PlayerPrefs.SetFloat("MusicVolume", _sliderMusic.value);
+        PlayerPrefs.Save();
+    }
+    
+    public void UpdateSoundValue()
+    {
+        foreach (var item in _audioSound)
+        {
+            item.volume = _sliderSound.value;
+        }
+
+        PlayerPrefs.SetFloat("SoundVolume", _sliderSound.value);
         PlayerPrefs.Save();
     }
 }
